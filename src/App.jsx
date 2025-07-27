ec288f5fe3c1aac87f52a4619fb3d7c00a4a8b3c
Renatha Putri <urneuross@gmail.com>
2025-07-28 06:52:15 +0700

    test

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+
+
+  return (
+    <div className="App bg-[#000300] min-h-screen">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
